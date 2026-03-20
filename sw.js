4d664eea26c237838162b3a42514b827513884cc
haroldco45 <68300211+haroldco45@users.noreply.github.com>
2026-03-20 05:14:16 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,18 @@
-self.addEventListener('install', (e) => {
-  console.log('SW instalado');
+const CACHE_NAME = 'burguer-cache-v1';
+const assets = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  // Manejo de peticiones básico
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
